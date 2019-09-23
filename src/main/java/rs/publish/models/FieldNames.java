1119ee285e9a4d1e6e3e015560f0084671788224
Satishsoft <satish.kumar@riversand.com>
2019-09-23 14:55:39 +0530

    removed all source files and added jar files

--- src/main/java/rs/publish/models/FieldNames.java
+++
@@ -1,9 +0,0 @@
-package rs.publish.models;
-
-public class FieldNames {
- public static String type="Type";
- public static String id="id";
- public static String name="Name";
- public static String childof_parentId="childof_parentId";
- public static String childof_parentType="childof_parentType";
-}
